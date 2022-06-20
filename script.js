3806a15c1c608f6d819425bd296a6b7be4a787b2
Lysa-Gomes <laysagomesmello9@gmail.com>
2022-06-20 12:52:00 -0300

    Aperendendo a ferramenta if or else

---
+++ script.js
@@ -0,0 +1,31 @@
+let idade = 18;
+if(idade >= 18){
+    console.log('Você é maior de idade');
+}
+else{
+    console.log('Você não é maior de idade')
+}
+ 
+let human = false
+if (idade>= 18 && human===true){
+    console.log('Você é um humano maior de idade')
+}
+else{
+    console.log('Você não possui os requisitos')
+}
+let mes = 'março'
+if( mes ==='janeiro' || mes ==='dezembro'){
+    console.log('Parabéns, feliz aniversário')
+}
+else{
+    console.log('Poxa, não é seu aniversário')
+}
+
+let nome = "Laysa";
+
+if( nome[0]==='R'){
+    console.log('Seu nome começa com R')
+}
+else{
+    console.log('Seu nome não começa com R')
+}
